ffd52c256d811f27ef6311385b889984673fa208
Abdullah Elmansy <abdullahelmansy03@gmail.com>
2026-03-31 12:20:35 +0200

    buil onboarding screen

--- lib/features/auth/presentation/screens/onboarding_screen.dart
+++ lib/features/auth/presentation/screens/onboarding_screen.dart
@@ -1,17 +1,224 @@
+import 'package:e_commerce_app/core/common/constants/app_constants.dart';
+import 'package:e_commerce_app/features/auth/presentation/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _controller = PageController();
+  int _currentPage = 0;
+
+  final List<OnboardingPage> _pages = [
+    OnboardingPage(
+      title: 'Discover Unique Products',
+      description: 'Find the perfect pieceto make you home truly yours',
+      image: 'assets/images/onboarding_img1.png',
+    ),
+    OnboardingPage(
+      title: 'Fast Delivery',
+      description: 'Get your favorite pies delivered to your doorstep',
+      image: 'assets/images/onboarding_img2.png',
+    ),
+    OnboardingPage(
+      title: 'Quality & Comfort',
+      description: 'Experience comfort with your high-quality pies',
+      image: 'assets/images/onboarding_img3.png',
+    ),
+  ];
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void _onNextPage() {
+    if (_currentPage < _pages.length - 1) {
+      _controller.nextPage(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      _completeOnboarding();
+    }
+  }
+
+  void _completeOnboarding() {
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => AuthScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeTransition(opacity: animation, child: child);
+        },
+        transitionDuration: Duration(milliseconds: 800),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Onboarding Screen',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
+      body: Stack(
+        children: [
+          PageView.builder(
+            itemBuilder: (context, index) {
+              return buildPage(_pages[index]);
+            },
+            itemCount: _pages.length,
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+          ),
+          //skip button
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 16,
+            right: 16,
+            child: TextButton(
+              onPressed: _completeOnboarding,
+              child: Text(
+                'Skip',
+                style: GoogleFonts.outfit(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          //bottom buttons
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //page indicator
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: AppConstants.primaryColor,
+                      border: Border.all(color: AppConstants.primaryColor),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        _pages.length,
+                        (index) => Container(
+                          margin: const EdgeInsets.only(right: 8),
+                          width: 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: _currentPage == index
+                                ? Colors.white
+                                : Colors.grey[500],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  //next and get started buttons
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppConstants.primaryColor,
+                      foregroundColor: Colors.white,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 32,
+                        vertical: 16,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          AppConstants.defaultBorderRadius,
+                        ),
+                      ),
+                    ),
+                    onPressed: _onNextPage,
+                    child: Text(
+                      _currentPage == _pages.length - 1
+                          ? 'Get Started'
+                          : 'Next',
+                      style: GoogleFonts.outfit(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  Widget buildPage(OnboardingPage page) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(),
+          Image.asset(
+            page.image,
+            height: MediaQuery.of(context).size.height * 0.4,
+            fit: BoxFit.contain,
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 24, left: 24, right: 24),
+            child: Column(
+              children: [
+                Text(
+                  page.title,
+                  style: GoogleFonts.outfit(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  page.description,
+                  style: GoogleFonts.outfit(
+                    fontSize: 14,
+                    height: 1.5,
+                    color: Colors.black87,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingPage {
+  final String title;
+  final String description;
+  final String image;
+
+  OnboardingPage({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
 }
